ba5f00d7789f525a70bd6ee85a973c813936c4f9
atharSPPS <34570080+atharSPPS@users.noreply.github.com>
2018-11-19 17:16:47 +0530

    Update App.java
    added by husain

--- GitEclipseTest/src/main/java/com/athar/GitEclipseTest/App.java
+++ GitEclipseTest/src/main/java/com/athar/GitEclipseTest/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Athar khan" );
+         System.out.println( "Athar Husain..........." );
     }
 }
