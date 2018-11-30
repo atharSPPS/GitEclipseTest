90215574a7667792c3f4cafaf1dc79660eab3493
atharSPPS <34570080+atharSPPS@users.noreply.github.com>
2018-11-30 16:30:23 +0530

    new file added

---
+++ GitEclipseTest/src/main/java/com/athar/GitEclipseTest/NewProduct.java
@@ -0,0 +1,18 @@
+package com.athar.GitEclipseTest;
+
+public class NewProduct {
+ Long Id;
+ String nameOfProduct;
+public Long getId() {
+	return Id;
+}
+public void setId(Long id) {
+	Id = id;
+}
+public String getNameOfProduct() {
+	return nameOfProduct;
+}
+public void setNameOfProduct(String nameOfProduct) {
+	this.nameOfProduct = nameOfProduct;
+}
+}
